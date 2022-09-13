ed6a1005b291c2ee1e5d228f9dd6b49dde61d87d
= <motasem.yakhni@gmail.com>
2022-09-13 10:55:32 +0300

    equation post request

--- equation.php
+++ equation.php
@@ -1,21 +1,15 @@
 <?php
 if(isset($_POST['a']) && isset($_POST['b']) && isset($_POST['c'])) {
-$a = $_POST['a'];
-$b = $_POST['b'];
-$c = $_POST['c'];
 
-function equation() {
- $equationSol = pow($a, 3) + $b*$c - $a/$b;
- $data = ['sol' => $equationSol];
- $postArr = ['http' => [
-            'method' => 'POST',
-            'header'=> "Content-type: application/x-www-form-urlencoded\r\n"
-                . "Content-Length: " . strlen($data) . "\r\n",
-            'content' => $data
-            )]
-];
-return json_encode($postArr);
-}
-echo equation();
+    function equation() {
+        $a = $_POST['a'];
+        $b = $_POST['b'];
+        $c = $_POST['c'];
+        $equationSol = pow($a, 3) + $b*$c - $a/$b;
+        $data = ['sol' => $equationSol];
+
+        return json_encode($data);
+    }
+    echo equation();
 }
 ?>
